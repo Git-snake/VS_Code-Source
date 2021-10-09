943d20b0ed5cabf5ba3862bb97c4ffa6576df087
Rick <295222984@qq.com>
2021-10-09 22:27:15 +0800

    master

---
+++ chapter05/动手实践/test.js
@@ -0,0 +1,6 @@
+var obj = 123;
+console.log(obj.toString());	// 输出结果：123
+obj = [1, 2, 3];
+console.log(obj.toString());	// 输出结果：1,2,3
+obj = function() {};
+console.log(obj.toString());	// 输出结果：function () {}
